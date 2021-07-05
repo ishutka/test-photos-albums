d73509182672af8eea94f965272c2a6d58e01e8c
Hanna Ish <ishutka.programmer@gmail.com>
2021-07-05 11:49:43 +0300

    set firebase config in buildModules

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,15 +1,39 @@
+const functions = require('firebase-functions');
+const firebaseConfigProject = (functions.config().projects || {}).zeboard || {};
 export default {
-    mode: 'spa',
+    mode: "spa",
 
     head: {
-        title: 'CC',
+        title: "CC",
         meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { charset: "utf-8" },
+            {
+                name: "viewport",
+                content: "width=device-width, initial-scale=1",
+            },
         ],
     },
 
     plugins: [],
 
-    buildModules: [],
+    buildModules: [
+        [
+            "@nuxtjs/firebase",
+            {
+                config: {
+                    apiKey: firebaseConfigProject.["api-key"] || process.env.FIREBASE_API_KEY,
+                    authDomain: "ishutka-zeboard.firebaseapp.com",
+                    projectId: "ishutka-zeboard",
+                    storageBucket: "ishutka-zeboard.appspot.com",
+                    messagingSenderId: firebaseConfigProject.["messaging-sender-id"] || process.env.FIREBASE_MESSAGING_SENDER_ID,
+                    appId: ((firebaseConfigProject.apps || {}).["test-photos-albums"] || {}).["measurement-id"] || process.env.FIREBASE_APP_ID,
+                    measurementId: ((firebaseConfigProject.apps || {}).["test-photos-albums"] || {}).["measurement-id"] || process.env.FIREBASE_MEASUREMENT_ID,
+                },
+                onFirebaseHosting: true,
+                services: {
+                    analytics: true,
+                },
+            },
+        ],
+    ],
 };
