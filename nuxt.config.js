2aa721b2655a447a467b1da20718172400204aec
Hanna Ish <ishutka.programmer@gmail.com>
2021-07-06 11:13:49 +0300

    test working version of firebase dev/prod adjusments

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,10 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 const firebaseConfigProject = (functions.config().projects || {}).zeboard || {};
+// require('dotenv').config()
+// console.log("process.env.NODE_EN: ", process.env.FIREBASE_API_KEY,process.env.NODE_ENV==='development');
 export default {
     mode: "spa",
-
+    dev: "develop",
     head: {
         title: "CC",
         meta: [
@@ -21,17 +23,30 @@ export default {
             "@nuxtjs/firebase",
             {
                 config: {
-                    apiKey: firebaseConfigProject.["api-key"] || process.env.FIREBASE_API_KEY,
+                    apiKey:
+                        firebaseConfigProject["api-key"] ||
+                        process.env.FIREBASE_API_KEY,
                     authDomain: "ishutka-zeboard.firebaseapp.com",
                     projectId: "ishutka-zeboard",
                     storageBucket: "ishutka-zeboard.appspot.com",
-                    messagingSenderId: firebaseConfigProject.["messaging-sender-id"] || process.env.FIREBASE_MESSAGING_SENDER_ID,
-                    appId: ((firebaseConfigProject.apps || {}).["test-photos-albums"] || {}).["measurement-id"] || process.env.FIREBASE_APP_ID,
-                    measurementId: ((firebaseConfigProject.apps || {}).["test-photos-albums"] || {}).["measurement-id"] || process.env.FIREBASE_MEASUREMENT_ID,
+                    messagingSenderId:
+                        firebaseConfigProject["messaging-sender-id"] ||
+                        process.env.FIREBASE_MESSAGING_SENDER_ID,
+                    appId:
+                        ((firebaseConfigProject.apps || {})[
+                            "test-photos-albums"
+                        ] || {})["measurement-id"] ||
+                        process.env.FIREBASE_APP_ID,
+                    measurementId:
+                        process.env.NODE_ENV != "development"
+                            ? ((firebaseConfigProject.apps || {})[
+                                  "test-photos-albums"
+                              ] || {})["measurement-id"]
+                            : "",
                 },
-                onFirebaseHosting: true,
+                onFirebaseHosting: process.env.NODE_ENV != "development",
                 services: {
-                    analytics: true,
+                    analytics: process.env.NODE_ENV != "development",
                 },
             },
         ],
