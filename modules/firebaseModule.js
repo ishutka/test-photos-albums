30eece785c560fb129de0094f1968d30917b0a79
Hanna Ish <ishutka.programmer@gmail.com>
2021-07-06 12:38:29 +0300

    creation and connection modules/firebaseModule.js

---
+++ modules/firebaseModule.js
@@ -0,0 +1,30 @@
+export default function() {
+    const { nuxt } = this;
+    if (nuxt.options.dev === false) {
+        return [
+            "@nuxtjs/firebase",
+            {
+                config: {
+                    apiKey: firebaseConfigProject["api-key"] || "",
+                    authDomain: "ishutka-zeboard.firebaseapp.com",
+                    projectId: "ishutka-zeboard",
+                    storageBucket: "ishutka-zeboard.appspot.com",
+                    messagingSenderId:
+                        firebaseConfigProject["messaging-sender-id"] || "",
+                    appId:
+                        ((firebaseConfigProject.apps || {})[
+                            "test-photos-albums"
+                        ] || {})["measurement-id"] || "",
+                    measurementId:
+                        ((firebaseConfigProject.apps || {})[
+                            "test-photos-albums"
+                        ] || {})["measurement-id"] || "",
+                },
+                onFirebaseHosting: true,
+                services: {
+                    analytics: true,
+                },
+            },
+        ];
+    }
+}
--- nuxt.config.js
+++ nuxt.config.js
@@ -19,36 +19,6 @@ export default {
     plugins: [],
 
     buildModules: [
-        [
-            "@nuxtjs/firebase",
-            {
-                config: {
-                    apiKey:
-                        firebaseConfigProject["api-key"] ||
-                        process.env.FIREBASE_API_KEY,
-                    authDomain: "ishutka-zeboard.firebaseapp.com",
-                    projectId: "ishutka-zeboard",
-                    storageBucket: "ishutka-zeboard.appspot.com",
-                    messagingSenderId:
-                        firebaseConfigProject["messaging-sender-id"] ||
-                        process.env.FIREBASE_MESSAGING_SENDER_ID,
-                    appId:
-                        ((firebaseConfigProject.apps || {})[
-                            "test-photos-albums"
-                        ] || {})["measurement-id"] ||
-                        process.env.FIREBASE_APP_ID,
-                    measurementId:
-                        process.env.NODE_ENV != "development"
-                            ? ((firebaseConfigProject.apps || {})[
-                                  "test-photos-albums"
-                              ] || {})["measurement-id"]
-                            : "",
-                },
-                onFirebaseHosting: process.env.NODE_ENV != "development",
-                services: {
-                    analytics: process.env.NODE_ENV != "development",
-                },
-            },
-        ],
+        '~/modules/firebaseModule.js'
     ],
 };
